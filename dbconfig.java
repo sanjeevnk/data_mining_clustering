b801171dc77d5b1543021d11cb0f2722824ec9f3
Sanjeev Kulkarni <kulkarni.msn@gmail.com>
2013-01-13 01:49:49 -0800

    Create dbconfig.java

---
+++ dbconfig.java
@@ -0,0 +1,25 @@
+package clustering;
+
+public class dbconfig {
+
+  
+	private static final String sid = "mydm";
+	private static final String port = "1521";
+	private static final String hostname = "localhost";
+	private static final String name = "sys as sysdba";
+	private static final String password = "sys";
+    
+	
+	public static String formedURI(){
+		return hostname+":"+port+":"+sid;
+	}
+
+	public static String getUsername() {
+		return name;
+	}
+
+	public static String getPassword() {
+		return password;
+	} 
+	
+}
